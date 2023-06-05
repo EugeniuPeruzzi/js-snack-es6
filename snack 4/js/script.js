ab13105d60cb334b687b07cc4720210862bc3904
EugeniuPeruzzi <jaka1997@live.it>
2023-06-05 16:21:59 +0200

    snack 4 , fixed

--- snack 4/js/script.js
+++ snack 4/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+let squadre = [];
+
+
+let nomiSquadre = ['Milan', 'Juventus', 'Inter', 'Verona' , 'Vicenza' , 'Venezia' , 'Pescara', 'Catanzaro'];
+
+nomiSquadre.forEach((nomeSquadra) => {
+
+  let squadra = {
+    nome: nomeSquadra,
+    puntiFatti: 0,
+    falliSubiti: 0
+  };
+
+
+  squadra.puntiFatti = Math.floor(Math.random() * 100);
+  squadra.falliSubiti = Math.floor(Math.random() * 50);
+
+  squadre.push(squadra);
+});
+
+
+let nomiEFalli = squadre.map(({ nome, falliSubiti }) => ({ nome, falliSubiti }));
+
+console.log(nomiEFalli);
